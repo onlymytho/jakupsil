30b69f6e20bb34dd9a31f8114a4cb871f9ac640b
Sanghyuk.Jung <onlymytho@gmail.com>
2017-11-21 23:20:12 +0900

    Change image zooming size

--- js/zoom_image.js
+++ js/zoom_image.js
@@ -1,7 +1,8 @@
 document.addEventListener( 'click', function( event ) {
     if (event.path[0].className == 'item_img') {
         zoom.to({
-            element: event.path[0]
+            element: event.path[0],
+            padding: 80
         })
     }
 });
